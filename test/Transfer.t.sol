4ce9fc7305552c127a58da70d2c226f97ab81845
dang1412 <dttung1412@gmail.com>
2024-08-21 19:16:45 +0900

    transfer test: update, check balance

--- test/Transfer.t.sol
+++ test/Transfer.t.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import "../src/lib/Transfer.sol";
 
 contract TransferTest is Test {
     TokenOwner mainchain = TokenOwner({
-        addr: 0x4Ab12E7CE31857Ee022f273e8580F73335a73c0B,
+        addr: 0xcf7eaaC7eE94261603e0600183EA2c739356cBf3,
         tokenAddr: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
         chainId: 1
     });
@@ -34,16 +35,16 @@ contract TransferTest is Test {
 
     function test_hash() view public {
         bytes32 h = Transfer.hash(receipt);
-        assertEq(h, 0x1e5e99eb46ee39b2e2c1debda52013111db220f0e57758c85696f8dd884b4937);
+        assertEq(h, 0xb577a130c54d89c4227872ae7413a1e8d76a3eedac8c5e8ba15e30b62cd8ca43);
     }
 
     function test_digest() pure public {
         bytes32 separator = 0x2b00d883a78a8b8d324cfe3cc451d9550b8509512a316b5dba270df178853071;
-        bytes32 receiptHash = 0x1e5e99eb46ee39b2e2c1debda52013111db220f0e57758c85696f8dd884b4937;
+        bytes32 receiptHash = 0xb577a130c54d89c4227872ae7413a1e8d76a3eedac8c5e8ba15e30b62cd8ca43;
 
         bytes32 digest = Transfer.receiptDigest(separator, receiptHash);
 
-        assertEq(digest, 0xd0281c1222b87dcf936168e477d7a4a6d494eba327cfa92514c60404cd55fc3f);
+        assertEq(digest, 0x2bfcedc9e24cb6e217f024e126af3c5bdd0647096d00f4aeb6a5b5cd9df979c6);
     }
 
     struct Signature {
@@ -52,24 +53,24 @@ contract TransferTest is Test {
         bytes32 s;
     }
 
-    function submitWithdrawal(Transfer.Receipt calldata _receipt, Signature[] calldata _signatures) external virtual returns (bool _locked) {
-        return false;
-    }
+    // function submitWithdrawal(Transfer.Receipt calldata _receipt, Signature[] calldata _signatures) external virtual returns (bool _locked) {
+    //     return false;
+    // }
 
     function test_RoninWithdraw() public {
-        // expect(sig.v).toBe(28)
-        // expect(sig.r).toBe('0x0db2950001044871c82a0a0e4dc48121fe8fde654a092b6d6cd46ffc2a645deb')
-        // expect(sig.s).toBe('0x2c9f1da40a4e166b061f174bd7ba4f2e487b9c2e92c9c1702f6b5da7fe99e835')
-        address calleeAddress = 0x64192819Ac13Ef72bF6b5AE239AC672B43a9AF08;
-        // Signature sig = Signature({});
+        // check balance before
+        address receiver = 0xcf7eaaC7eE94261603e0600183EA2c739356cBf3;
+        assertEq(receiver.balance, 332360355140776000);
+
+        address bridgeAddress = 0x64192819Ac13Ef72bF6b5AE239AC672B43a9AF08;
 
         uint8 v = 28;
-        bytes32 r = 0x897058e20ee89766649370ec29a43ddef9543d3923488eecaf6b04c26627b522;
-        bytes32 s = 0x23e9fc3d539039e06e741b84c9fe2ad263c27fef5b63e85e86f2206acc6b2f6e;
+        bytes32 r = 0xd44cf54b611a01d097b8315b1675dfecb9950b3cdaa71c65f233a84e87fe554b;
+        bytes32 s = 0x2e5dfbd5d8a750a267bc7279c3944235453949684726bcd0755ef37bbf8f3777;
 
         Signature[] memory signatures = new Signature[](1);
         signatures[0] = Signature({v: v, r: r, s: s});
-        (bool success, bytes memory data) = calleeAddress.call(
+        (bool success, bytes memory data) = bridgeAddress.call(
             abi.encodeWithSignature(
                 "submitWithdrawal((uint256,uint8,(address,address,uint256),(address,address,uint256),(uint8,uint256,uint256)),(uint8,bytes32,bytes32)[])",
                 receipt,
@@ -79,6 +80,11 @@ contract TransferTest is Test {
 
         bool result = abi.decode(data, (bool));
         assertEq(success, true);
-        assertEq(result, true);
+        assertEq(result, false);
+
+        // check balance after
+        // IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        // uint256 balance = WETH.balanceOf(receiver);
+        assertEq(receiver.balance, 3996426110355140776000);
     }
 }
